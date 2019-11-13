4bb80b43b1f5b3c5c6db56af2956194caeedcfaa
JJIKKYU <jjikkyu@naver.com>
2019-11-14 02:22:34 +0900

    Add : UI Design

--- BlackWall/Source/BlackWall/BlackWallCharacter.cpp
+++ BlackWall/Source/BlackWall/BlackWallCharacter.cpp
@@ -32,8 +32,8 @@ ABlackWallCharacter::ABlackWallCharacter()
 	, bCanDash(true), bDashing(false), bDashStop(0.15f), mDashDistance(6000.f), mDashCollDown(.5f), mDashUsingMP(15.f)
 
 	// HP & MP
-	, mMaxHP(8.f), mHP(4.f), mHPrecoveryRate(.1f)
-	, mMaxMP(10.f), mMP(2.f), mMPrecoveryRate(.5f)
+	, mMaxHP(100.f), mHP(85.f), mHPrecoveryRate(.1f)
+	, mMaxMP(100.f), mMP(50.f), mMPrecoveryRate(.5f)
 
 	// temporary var
 	, bWeaponEquipped(false)
@@ -96,12 +96,10 @@ void ABlackWallCharacter::Tick(float DeltaTime)
 	if (MovementStatus == EMovementStatus::EMS_Dead) return;
 
 	// Auto Recovery HP, MP
-	/*
 	float DeltaMP = mMPrecoveryRate * DeltaTime;
 	float DeltaHP = mHPrecoveryRate * DeltaTime;
 	mMP += DeltaMP;
 	mHP += DeltaHP;
-	*/
 }
 
 void ABlackWallCharacter::setMovementStatus(EMovementStatus status)
